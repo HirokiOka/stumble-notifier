c3e7dc8e1f803e698b5f4a651bc91e598a76d394
hiroki <hiro_bass.0827@icloud.com>
2022-07-14 17:37:16 +0900

    add dynamic rooting

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+title = "Stumble Notifier"
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    ids = [1, 2, 3, 4, 5, 6]
+    return render_template('index.html', title=title, ids=ids)
+
+
+@app.route('/student/<id>')
+def student_data(id):
+    data = [
+            {"time": "10:10", "sloc": 4, "ed": 2},
+            {"time": "10:12", "sloc": 7, "ed": 3},
+            {"time": "10:20", "sloc": 4, "ed": 5}
+    ]
+    return render_template('student_data.html', title=title, id=id, data=data)
 
 
 if __name__ == '__main__':
