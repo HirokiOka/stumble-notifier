5318b2a1b9a14cb4af54efe1ff769e314fdc2216
hiroka <hiroka@hirokanoMacBook-Air.local>
2022-10-25 09:57:21 +0900

    fix time

--- static/js/fetcher.js
+++ static/js/fetcher.js
@@ -1,16 +1,20 @@
-const intervalMiliSec = 5000;
+const intervalMiliSec = 1000;
 const URL = "https://stumble-notifier.herokuapp.com/data";
 const ids = ["2160897H", "2181076H", "2191201H"];
 ids.sort();
 
 
 setInterval(async () => {
-  const result = await fetch(URL).then(res => res.json());
-  console.log(result);
-  ids.forEach((v, i) => {
-    const codeEle = document.getElementById(`${v}-code`);
-    const multiEle = document.getElementById(`${v}-multi`);
-    codeEle.style.background = result[i][0] ? 'red' : 'white';
-    multiEle.style.background = result[i][1] ? 'red' : 'white';
-  });
+  try {
+    const result = await fetch(URL).then(res => res.json());
+    console.log(result);
+    ids.forEach((v, i) => {
+      const codeEle = document.getElementById(`${v}-code`);
+      const multiEle = document.getElementById(`${v}-multi`);
+      codeEle.style.background = result[i][0] ? 'red' : 'white';
+      multiEle.style.background = result[i][1] ? 'red' : 'white';
+    });
+  } catch(e) {
+    console.log(e);
+  }
 }, intervalMiliSec);
